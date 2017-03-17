5fa8ed99b1a9f5dc44196eb86834013f1008c5d7
lrinQVQ <cqlrin@gmail.com>
2017-03-17 10:31:31 +0800

    update
    Later added commit message

--- q39.mk
+++ q39.mk
@@ -136,27 +136,131 @@ PRODUCT_COPY_FILES += \
 
 # acdb
 PRODUCT_COPY_FILES += \
-    device/tcl/q39/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Bluetooth_cal.acdb:system/etc/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Bluetooth_cal.acdb \
-    device/tcl/q39/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_General_cal.acdb:system/etc/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_General_cal.acdb \
-    device/tcl/q39/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Global_cal.acdb:system/etc/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Global_cal.acdb \
-    device/tcl/q39/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Handset_cal.acdb:system/etc/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Handset_cal.acdb \
-    device/tcl/q39/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Hdmi_cal.acdb:system/etc/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Hdmi_cal.acdb \
-    device/tcl/q39/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Headset_cal.acdb:system/etc/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Headset_cal.acdb \
-    device/tcl/q39/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Speaker_cal.acdb:system/etc/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Speaker_cal.acdb \
-    device/tcl/q39/acdbdata/MTP/MTP_Bluetooth_cal.acdb:system/etc/acdbdata/MTP/MTP_Bluetooth_cal.acdb \
-    device/tcl/q39/acdbdata/MTP/MTP_General_cal.acdb:system/etc/acdbdata/MTP/MTP_General_cal.acdb \
-    device/tcl/q39/acdbdata/MTP/MTP_Global_cal.acdb:system/etc/acdbdata/MTP/MTP_Global_cal.acdb \
-    device/tcl/q39/acdbdata/MTP/MTP_Handset_cal.acdb:system/etc/acdbdata/MTP/MTP_Handset_cal.acdb \
-    device/tcl/q39/acdbdata/MTP/MTP_Hdmi_cal.acdb:system/etc/acdbdata/MTP/MTP_Hdmi_cal.acdb \
-    device/tcl/q39/acdbdata/MTP/MTP_Headset_cal.acdb:system/etc/acdbdata/MTP/MTP_Headset_cal.acdb \
-    device/tcl/q39/acdbdata/MTP/MTP_Speaker_cal.acdb:system/etc/acdbdata/MTP/MTP_Speaker_cal.acdb \
-    device/tcl/q39/acdbdata/QRD/QRD_Bluetooth_cal.acdb:system/etc/acdbdata/QRD/QRD_Bluetooth_cal.acdb \
-    device/tcl/q39/acdbdata/QRD/QRD_General_cal.acdb:system/etc/acdbdata/QRD/QRD_General_cal.acdb \
-    device/tcl/q39/acdbdata/QRD/QRD_Global_cal.acdb:system/etc/acdbdata/QRD/QRD_Global_cal.acdb \
-    device/tcl/q39/acdbdata/QRD/QRD_Handset_cal.acdb:system/etc/acdbdata/QRD/QRD_Handset_cal.acdb \
-    device/tcl/q39/acdbdata/QRD/QRD_Hdmi_cal.acdb:system/etc/acdbdata/QRD/QRD_Hdmi_cal.acdb \
-    device/tcl/q39/acdbdata/QRD/QRD_Headset_cal.acdb:system/etc/acdbdata/QRD/QRD_Headset_cal.acdb \
-    device/tcl/q39/acdbdata/QRD/QRD_Speaker_cal.acdb:system/etc/acdbdata/QRD/QRD_Speaker_cal.acdb
+    device/tcl/q39/config/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Bluetooth_cal.acdb:system/etc/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Bluetooth_cal.acdb \
+    device/tcl/q39/config/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_General_cal.acdb:system/etc/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_General_cal.acdb \
+    device/tcl/q39/config/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Global_cal.acdb:system/etc/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Global_cal.acdb \
+    device/tcl/q39/config/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Handset_cal.acdb:system/etc/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Handset_cal.acdb \
+    device/tcl/q39/config/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Hdmi_cal.acdb:system/etc/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Hdmi_cal.acdb \
+    device/tcl/q39/config/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Headset_cal.acdb:system/etc/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Headset_cal.acdb \
+    device/tcl/q39/config/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Speaker_cal.acdb:system/etc/acdbdata/MTP/msm8939-tapan-snd-card/MTP_WCD9306_Speaker_cal.acdb \
+    device/tcl/q39/config/acdbdata/MTP/MTP_Bluetooth_cal.acdb:system/etc/acdbdata/MTP/MTP_Bluetooth_cal.acdb \
+    device/tcl/q39/config/acdbdata/MTP/MTP_General_cal.acdb:system/etc/acdbdata/MTP/MTP_General_cal.acdb \
+    device/tcl/q39/config/acdbdata/MTP/MTP_Global_cal.acdb:system/etc/acdbdata/MTP/MTP_Global_cal.acdb \
+    device/tcl/q39/config/acdbdata/MTP/MTP_Handset_cal.acdb:system/etc/acdbdata/MTP/MTP_Handset_cal.acdb \
+    device/tcl/q39/config/acdbdata/MTP/MTP_Hdmi_cal.acdb:system/etc/acdbdata/MTP/MTP_Hdmi_cal.acdb \
+    device/tcl/q39/config/acdbdata/MTP/MTP_Headset_cal.acdb:system/etc/acdbdata/MTP/MTP_Headset_cal.acdb \
+    device/tcl/q39/config/acdbdata/MTP/MTP_Speaker_cal.acdb:system/etc/acdbdata/MTP/MTP_Speaker_cal.acdb \
+    device/tcl/q39/config/acdbdata/QRD/QRD_Bluetooth_cal.acdb:system/etc/acdbdata/QRD/QRD_Bluetooth_cal.acdb \
+    device/tcl/q39/config/acdbdata/QRD/QRD_General_cal.acdb:system/etc/acdbdata/QRD/QRD_General_cal.acdb \
+    device/tcl/q39/config/acdbdata/QRD/QRD_Global_cal.acdb:system/etc/acdbdata/QRD/QRD_Global_cal.acdb \
+    device/tcl/q39/config/acdbdata/QRD/QRD_Handset_cal.acdb:system/etc/acdbdata/QRD/QRD_Handset_cal.acdb \
+    device/tcl/q39/config/acdbdata/QRD/QRD_Hdmi_cal.acdb:system/etc/acdbdata/QRD/QRD_Hdmi_cal.acdb \
+    device/tcl/q39/config/acdbdata/QRD/QRD_Headset_cal.acdb:system/etc/acdbdata/QRD/QRD_Headset_cal.acdb \
+    device/tcl/q39/config/acdbdata/QRD/QRD_Speaker_cal.acdb:system/etc/acdbdata/QRD/QRD_Speaker_cal.acdb
+
+# firmware
+PRODUCT_COPY_FILES += \
+    device/tcl/q39/config/firmware/wcd9306/wcd9306_anc.bin:system/etc/firmware/wcd9306/wcd9306_anc.bin \
+    device/tcl/q39/config/firmware/wcd9306/wcd9306_mbhc.bin:system/etc/firmware/wcd9306/wcd9306_mbhc.bin \
+    device/tcl/q39/config/firmware/a225p5_pm4.fw:system/etc/firmware/a225p5_pm4.fw \
+    device/tcl/q39/config/firmware/a225_pfp.fw:system/etc/firmware/a225_pfp.fw \
+    device/tcl/q39/config/firmware/a225_pm4.fw:system/etc/firmware/a225_pm4.fw \
+    device/tcl/q39/config/firmware/a300_pfp.fw:system/etc/firmware/a300_pfp.fw \
+    device/tcl/q39/config/firmware/a300_pm4.fw:system/etc/firmware/a300_pm4.fw \
+    device/tcl/q39/config/firmware/a330_pfp.fw:system/etc/firmware/a330_pfp.fw \
+    device/tcl/q39/config/firmware/a330_pm4.fw:system/etc/firmware/a330_pm4.fw \
+    device/tcl/q39/config/firmware/a420_pfp.fw:system/etc/firmware/a420_pfp.fw \
+    device/tcl/q39/config/firmware/a420_pm4.fw:system/etc/firmware/a420_pm4.fw \
+    device/tcl/q39/config/firmware/cpp_firmware_v1_1_1.fw:system/etc/firmware/cpp_firmware_v1_1_1.fw \
+    device/tcl/q39/config/firmware/cpp_firmware_v1_1_6.fw:system/etc/firmware/cpp_firmware_v1_1_6.fw \
+    device/tcl/q39/config/firmware/cpp_firmware_v1_2_0.fw:system/etc/firmware/cpp_firmware_v1_2_0.fw \
+    device/tcl/q39/config/firmware/cpp_firmware_v1_4_0.fw:system/etc/firmware/cpp_firmware_v1_4_0.fw \
+    device/tcl/q39/config/firmware/ice40.bin:system/etc/firmware/ice40.bin \
+    device/tcl/q39/config/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
+    device/tcl/q39/config/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
+    device/tcl/q39/config/firmware/Signedrompatch_v20.bin:system/etc/firmware/Signedrompatch_v20.bin \
+    device/tcl/q39/config/firmware/Signedrompatch_v21.bin:system/etc/firmware/Signedrompatch_v21.bin \
+    device/tcl/q39/config/firmware/Signedrompatch_v24.bin:system/etc/firmware/Signedrompatch_v24.bin \
+    device/tcl/q39/config/firmware/Signedrompatch_v30.bin:system/etc/firmware/Signedrompatch_v30.bin \
+    device/tcl/q39/config/firmware/venus.b00:system/etc/firmware/venus.b00 \
+    device/tcl/q39/config/firmware/venus.b01:system/etc/firmware/venus.b01 \
+    device/tcl/q39/config/firmware/venus.b02:system/etc/firmware/venus.b02 \
+    device/tcl/q39/config/firmware/venus.b03:system/etc/firmware/venus.b03 \
+    device/tcl/q39/config/firmware/venus.b04:system/etc/firmware/venus.b04 \
+    device/tcl/q39/config/firmware/venus.mbn:system/etc/firmware/venus.mbn \
+    device/tcl/q39/config/firmware/venus.mdt:system/etc/firmware/venus.mdt
+
+# cacert
+PRODUCT_COPY_FILES += \
+    device/tcl/q39/config/cacert_location.pem:system/etc/cacert_location.pem
+
+# config
+PRODUCT_COPY_FILES += \
+    device/tcl/q39/config/capability.xml:system/etc/capability.xml \
+    device/tcl/q39/config/cdma_call_conf.xml:system/etc/cdma_call_conf.xml \
+    device/tcl/q39/config/ftm_test_config:system/etc/ftm_test_config \
+    device/tcl/q39/config/ftm_test_config_mtp:system/etc/ftm_test_config_mtp \
+    device/tcl/q39/config/ftm_test_config_wcd9306:system/etc/ftm_test_config_wcd9306 \
+    device/tcl/q39/config/lowi.conf:system/etc/lowi.conf \
+    device/tcl/q39/config/qmi_fw.conf:system/etc/qmi_fw.conf \
+    device/tcl/q39/config/spn-conf.xml:system/etc/spn-conf.xml \
+    device/tcl/q39/config/wfdconfigsink.xml:system/etc/wfdconfigsink.xml \
+    device/tcl/q39/config/wfdconfig.xml:system/etc/wfdconfig.xml \
+    device/tcl/q39/config/xtra_root_cert.pem:system/etc/xtra_root_cert.pem \
+    device/tcl/q39/config/xtwifi.conf:system/etc/xtwifi.conf
+
+# surround sound
+PRODUCT_COPY_FILES += \
+    device/tcl/q39/config/surround_sound/filter1i.pcm:system/etc/surround_sound/filter1i.pcm \
+    device/tcl/q39/config/surround_sound/filter1r.pcm:system/etc/surround_sound/filter1r.pcm \
+    device/tcl/q39/config/surround_sound/filter2i.pcm:system/etc/surround_sound/filter2i.pcm \
+    device/tcl/q39/config/surround_sound/filter2r.pcm:system/etc/surround_sound/filter2r.pcm \
+    device/tcl/q39/config/surround_sound/filter3i.pcm:system/etc/surround_sound/filter3i.pcm \
+    device/tcl/q39/config/surround_sound/filter3r.pcm:system/etc/surround_sound/filter3r.pcm \
+    device/tcl/q39/config/surround_sound/filter4i.pcm:system/etc/surround_sound/filter4i.pcm \
+    device/tcl/q39/config/surround_sound/filter4r.pcm:system/etc/surround_sound/filter4r.pcm
+
+# cne
+PRODUCT_COPY_FILES += \
+    device/tcl/q39/config/cne/andsfCne.xml:system/etc/cne/andsfCne.xml \
+    device/tcl/q39/config/cne/SwimConfig.xml:system/etc/cne/SwimConfig.xml \
+    device/tcl/q39/config/cne/wqeclient/profile1.xml:system/etc/cne/wqeclient/profile1.xml \
+    device/tcl/q39/config/cne/wqeclient/profile2.xml:system/etc/cne/wqeclient/profile2.xml \
+    device/tcl/q39/config/cne/wqeclient/profile3.xml:system/etc/cne/wqeclient/profile3.xml \
+    device/tcl/q39/config/cne/wqeclient/profile4.xml:system/etc/cne/wqeclient/profile4.xml \
+    device/tcl/q39/config/cne/wqeclient/profile5.xml:system/etc/cne/wqeclient/profile5.xml
+
+# dpm
+PRODUCT_COPY_FILES += \
+    device/tcl/q39/config/dpm/dpm.conf:system/etc/dpm/dpm.conf \
+    device/tcl/q39/config/dpm/nsrm/NsrmConfiguration.xml:system/etc/dpm/nsrm/NsrmConfiguration.xml
+
+# mmi
+PRODUCT_COPY_FILES += \
+    device/tcl/q39/config/mmi/fail.png:system/etc/mmi/fail.png \
+    device/tcl/q39/config/mmi/fonts.ttf:system/etc/mmi/fonts.ttf \
+    device/tcl/q39/config/mmi/pass.png:system/etc/mmi/pass.png \
+    device/tcl/q39/config/mmi/strings.xml:system/etc/mmi/strings.xml \
+    device/tcl/q39/config/mmi/strings-zh-rCN.xml:system/etc/mmi/strings-zh-rCN.xml
+
+# permissions
+PRODUCT_COPY_FILES += \
+    device/tcl/q39/config/permissions/btmultisim.xml:system/etc/permissions/btmultisim.xml \
+    device/tcl/q39/config/permissions/cneapiclient.xml:system/etc/permissions/cneapiclient.xml \
+    device/tcl/q39/config/permissions/com.qrd.wappush.xml:system/etc/permissions/com.qrd.wappush.xml \
+    device/tcl/q39/config/permissions/com.qti.dpmframework.xml:system/etc/permissions/com.qti.dpmframework.xml \
+    device/tcl/q39/config/permissions/com.qti.snapdragon.sdk.display.xml:system/etc/permissions/com.qti.snapdragon.sdk.display.xml \
+    device/tcl/q39/config/permissions/com.qualcomm.qmapbridge.xml:system/etc/permissions/com.qualcomm.qmapbridge.xml \
+    device/tcl/q39/config/permissions/com.qualcomm.qti.smartsearch.xml:system/etc/permissions/com.qualcomm.qti.smartsearch.xml \
+    device/tcl/q39/config/permissions/com.quicinc.cne.xml:system/etc/permissions/com.quicinc.cne.xml \
+    device/tcl/q39/config/permissions/ConnectivityExt.xml:system/etc/permissions/ConnectivityExt.xml \
+    device/tcl/q39/config/permissions/dpmapi.xml:system/etc/permissions/dpmapi.xml \
+    device/tcl/q39/config/permissions/embms.xml:system/etc/permissions/embms.xml \
+    device/tcl/q39/config/permissions/imscm.xml:system/etc/permissions/imscm.xml \
+    device/tcl/q39/config/permissions/qcnvitems.xml:system/etc/permissions/qcnvitems.xml \
+    device/tcl/q39/config/permissions/qcrilhook.xml:system/etc/permissions/qcrilhook.xml \
+    device/tcl/q39/config/permissions/qti_permissions.xml:system/etc/permissions/qti_permissions.xml \
+    device/tcl/q39/config/permissions/rcsimssettings.xml:system/etc/permissions/rcsimssettings.xml
 
 #wlan driver
 PRODUCT_COPY_FILES += \
